09c72251dc239d280cf611249a30affbfed684a6
sivabalanmano <siva08082001@gmail.com>
2023-11-18 18:29:28 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import Header from "./Header";
+import '/node_modules/bootstrap/dist/css/bootstrap.min.css';
+// import "./main.css";
+
+function App() {
+  return (
+    <div>
+
+       <Header/>
+
+    </div>
+  );
+}
+
+export default App;
